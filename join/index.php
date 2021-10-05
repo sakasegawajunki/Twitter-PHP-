52aed6bf6dc3ac03d534f1d18f76765edb6216e1
sakasegawajunki <80205010+sakasegawajunki@users.noreply.github.com>
2021-10-05 19:10:19 +0900

    重複登録を防止

--- join/index.php
+++ join/index.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+require("../dbconnect.php");
 if (!empty($_POST)) {
 
 	if ($_POST["name"] === "") {
@@ -20,6 +21,15 @@ if (!empty($_POST)) {
 		$ext = substr($fileName, -3);
 		if ($ext != "jpg" && $ext != "png" && $ext != "gif") {
 			$error["image"] = "type";
+		}
+	}
+
+	if(empty($error)) {
+		$member = $db->prepare("SELECT COUNT(*) AS cnt FROM members WHERE email=?");
+		$member->execute(array($_POST["email"]));
+		$record = $member->fetch();
+		if($record["cnt"] > 0) {
+			$error["email"] = "duplicate";
 		}
 	}
 
@@ -73,6 +83,9 @@ if ($_REQUEST["action"] == "rewrite" && isset($_SESSION["join"])) {
 						<input type="text" name="email" size="35" maxlength="255" value="<?php print(htmlspecialchars($_POST["email"], ENT_QUOTES)); ?>" />
 						<?php if ($error["email"] === "blank") : ?>
 							<P class="error">*　メールアドレスを入力してください。</P>
+						<?php endif; ?>
+						<?php if ($error["email"] === "duplicate") : ?>
+							<P class="error">*　指定されたメールアドレスは、すでに登録されています。</P>
 						<?php endif; ?>
 					<dt>パスワード<span class="required">必須</span></dt>
 					<dd>
